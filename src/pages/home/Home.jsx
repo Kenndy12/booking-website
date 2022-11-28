6a0ed1bc7289bf37065087a0dbfe627688cf9bb3
Kenndy12 <justkenndy92@gmail.com>
2022-11-28 15:32:20 +0700

    test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,9 +13,9 @@ const Home = () => {
         <Header/>
         <div className="homeContainer">
             <Featured/>
-            <h1 className="homeTitle">Browse by property types</h1>
+            <h1 className="homeTitle">Browse by property type</h1>
             <PropertyList/>
-            <h1 className="homeTitle">Homes guest loves</h1>
+            <h1 className="homeTitle">Homes guest love</h1>
             <FeaturedProperties/>
             <MailList/>
         </div>
