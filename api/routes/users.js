48867849acb872909adbedf30780284057778724
Kenndy12 <justkenndy92@gmail.com>
2022-11-30 14:39:28 +0700

    first commit

---
+++ api/routes/users.js
@@ -0,0 +1,19 @@
+import express  from "express";
+import { deleteUser, getAllUser, getUser, updateUser } from "../controller/user.js";
+
+const router = express.Router();
+
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getAllUser);
+
+
+export default router;
